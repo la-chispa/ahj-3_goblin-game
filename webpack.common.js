2318e5df9d459d641d719ae04c83dde9ba4d6513
Olga Belotserkovskaya <olla.chispa@gmail.com>
2021-03-09 22:29:35 +0600

    try to fix webpack one more time

--- webpack.common.js
+++ webpack.common.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    // filename: 'bundle.js',
+    publicPath: '',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
